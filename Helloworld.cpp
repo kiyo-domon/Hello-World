6c3876d7e2bde26a1b3a4c8c4648053279d4a4d5
kiyo-domon <Kiyo@Renes-MacBook-Pro-4.local>
2015-05-18 17:49:40 -0500

    testing lasst

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main()
-<<<<<<< HEAD
+
 {
 
-//commentis
-=======
-{//commentis
->>>>>>> e394119e91f51eef8989596dc4a903c9db6d5c2d
+	//messed up one commits and such dont know how oh well
+	
 	cout << "Hello world please insert gutuer"
 	return 0;
 }
